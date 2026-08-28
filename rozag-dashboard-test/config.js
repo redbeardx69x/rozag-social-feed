7ef450a10f0c179b4f61a3dbc29500775d3791f3
RoZAG <red.beardx69x@gmail.com>
2026-08-28 13:43:37 +0200

    Update config.js

--- rozag-dashboard-test/config.js
+++ rozag-dashboard-test/config.js
@@ -1,5 +1,5 @@
 window.ROZAG_DASHBOARD_CONFIG = Object.freeze({
-  AUTH_START_URL: "https://rozag.coolvetspaces.com/dashboard-test/login",
-  AUTH_ME_URL: "https://rozag.coolvetspaces.com/dashboard-test/api/me",
-  LOGOUT_URL: "https://rozag.coolvetspaces.com/dashboard-test/logout"
+  AUTH_START_URL: "https://rozag.coolvetspaces.com/dashboard/login",
+  AUTH_ME_URL: "https://rozag.coolvetspaces.com/dashboard/api/me",
+  LOGOUT_URL: "https://rozag.coolvetspaces.com/dashboard/logout"
 });
