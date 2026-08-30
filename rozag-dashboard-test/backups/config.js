dc7c78255ee79dcc4f8086ce0d0f0614acd59d89
RoZAG <red.beardx69x@gmail.com>
2026-08-30 14:18:09 +0200

    Add files via upload

---
+++ rozag-dashboard-test/backups/config.js
@@ -0,0 +1,5 @@
+window.ROZAG_DASHBOARD_CONFIG = Object.freeze({
+  AUTH_START_URL: "https://rozag.coolvetspaces.com/dashboard/login",
+  AUTH_ME_URL: "https://rozag.coolvetspaces.com/dashboard/api/me",
+  LOGOUT_URL: "https://rozag.coolvetspaces.com/dashboard/logout"
+});
